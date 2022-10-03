07e89fad59be59bd3f80296393d85eccecc7fcfd
David19782 <yordanovdavid@gmail.com>
2022-10-03 17:38:51 +0300

    feat: correct app

--- src/js/Application.js
+++ src/js/Application.js
@@ -7,19 +7,28 @@ export default class Application extends EventEmitter {
       READY: "ready",
     };
   }
-
   constructor() {
     super();
-
+    
+    this._beat = new Beat()
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
-
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
-
-    document.querySelector(".main").appendChild(message);
-
+    
+    
+    this._beat.addListener('bit', (e) => {
+      count %= 6;
+      this._create(lyrics[count])
+      count++;
+    })
+  
     this.emit(Application.events.READY);
   }
+
+  _create(mes) {
+    const message = document.createElement("div");
+    message.classList.add("message");
+    message.innerText = mes;
+    document.querySelector(".main").appendChild(message);
+  }
+
 }
